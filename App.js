55d2953f5ecdf24eea05e5f71481dfc99356ab55
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-13 05:35:16 -0500

    generating hex  code using random number

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 const getColor = () => {
   const getNumber = Math.floor(Math.random()*16777215);
-  // Floor karta ye hai ye minimum integer return karta hai     
-  console.log(getNumber);
+  const getCode="#"+getNumber.toString(16);
+  // toString Method basically string ko jaisi hai vaisi return kar deta hai
+  // 16 (hex unit) pass karane ka fayda ye hua kii toString humko avv normal 
+  // string return na karke uss normal string ko hexadeciaml mai return karega
+  // aur phir wo concatenated ho legi string # wali string se 
+  console.log(getNumber,getCode);
 };
 
-// Above function will generate  a random integer for U 
-// Adding click event listener on btn and call the get color method 
+
+
 document.getElementById("btn").addEventListener("click", getColor);
